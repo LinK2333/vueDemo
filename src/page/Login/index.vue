8b66084f0ababe731072326240c37461a52328cb
linkai <754706820@qq.com>
2019-11-09 10:12:45 +0800

    upLodeLogin

--- src/page/Login/index.vue
+++ src/page/Login/index.vue
@@ -1,16 +1,17 @@
 <template>
   <el-row class="row" type="flex" justify="center" align="middle">
     <el-col :span="8">
-      <el-form class="form" :model="loginForm" :rules="rules" ref="loginForm">
+      <el-form :model="loginForm" :rules="rules" ref="loginForm" class="demo-loginForm">
         <el-form-item label="用户名" prop="username">
-          <el-input v-model="loginForm.username"></el-input>
+          <el-input v-model="loginForm.username" placeholder="请输入用户名"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input v-model="loginForm.password"></el-input>
+          <el-input v-model="loginForm.password" placeholder="请输入密码"></el-input>
         </el-form-item>
+
         <el-form-item>
-          <el-button type="primary">登录</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="starLogin('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -22,15 +23,32 @@ export default {
   data () {
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       },
-      rules: {}
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 6 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 5, max: 10, message: '长度在 5 到 10 个字符', trigger: 'blur' }
+        ]
+      }
     }
   },
-  methods: {}
+  methods: {
+    starLogin () {
+      this.$refs.loginForm.validate(valid => {})
+    },
+    resetForm () {
+      this.$refs.loginForm.resetFields()
+    }
+  }
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+@import "@/style/login.scss";
 </style>
